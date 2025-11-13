c2c1882210e26f28f49fb514d5b5f2ff663c6db9
SamirDawar <samir.dawar01@gmail.com>
2025-11-12 23:18:47 -0500

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "game_101_prototype",
+  base: "/game_101_prototype/",
   plugins: [react(), tailwindcss()],
 })
